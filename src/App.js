186dd5260d34d03d92e32281e7469751aa8f1507
admiralpxl <wnstnjr@gmail.com>
2021-10-14 14:52:07 -0500

    Input Search component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  /*const [equipos, setEquipos] = React.useState([]);
+
+  const obtenerDatos = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setEquipos(data);
+    console.log(equipos);
+  };
+
+  React.useEffect(() => {
+    obtenerDatos();
+  }, []);*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        Hola como estas, ingresa el nombre del usuario de github que quieras
+        saber
+      </h2>
+      <input type="text" />
+      <button> Buscar </button>
     </div>
   );
 }
 
-export default App;
+export { App };
---
+++ src/FetchAPI.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FetchAPI() {
+  return <p></p>;
+}
+
+export { FetchAPI };
